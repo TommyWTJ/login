cedc65b6afb00ee6105676405dc86b138ef98ea7
TommyWTJ <wtjuwt123@gmail.com>
2021-10-01 16:01:29 -0700

    Initial commit

---
+++ src/main/java/com/wtj/login/LoginApplication.java
@@ -0,0 +1,13 @@
+package com.wtj.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wtj/login/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wtj.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
